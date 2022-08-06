91485d20d451d4581d5aa66483adb5015fbe21af
Metin DEMİR <metindemir1215.md@gmail.com>
2022-08-06 14:48:20 +0300

    Boombland frame added

---
+++ src/com/nazo/bombland/Main.java
@@ -0,0 +1,12 @@
+package com.nazo.bombland;
+
+import com.nazo.bombland.gui.BomblandFrame;
+
+public class Main {
+
+
+    public  static void  main(String args[]){
+
+        new BomblandFrame().setVisible(true);
+    }
+}
---
+++ src/com/nazo/bombland/gui/BomblandFrame.java
@@ -0,0 +1,14 @@
+package com.nazo.bombland.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BomblandFrame extends JFrame {
+
+
+    private Dimension dimension=new Dimension(500,500);
+
+    public BomblandFrame(){
+        setSize(dimension);
+    }
+}
